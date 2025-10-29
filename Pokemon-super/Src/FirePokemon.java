ae620ba4d04bf130aad4784f6942006a568c559c
eabr7 <e.bruins@protonmail.com>
2025-10-29 14:15:06 +0100

    added variables, constructors, getters, setters, methods, and override

--- Pokemon-super/Src/FirePokemon.java
+++ Pokemon-super/Src/FirePokemon.java
@@ -1,4 +1,51 @@
 class FirePokemon extends PokemonSuper {
+    // variables, minimum of 2 private.
+    private int flameTemperature;
+    private String flameColor;
 
+    // constructor, minimum of 1.
 
+    public FirePokemon(String name, int level, int hp, int xp, String food, String sound, int flameTemperature, String flameColor) {
+        super(name, level, hp, xp, food, sound);
+        this.flameTemperature = flameTemperature;
+        this.flameColor = flameColor;
+    }
+
+    // getters, minimum of 2.
+    public int getFlameTemperature() {
+        return flameTemperature;
+    }
+
+    public String getFlameColor() {
+        return flameColor;
+    }
+
+    //setters, minimum of 2.
+    public void setFlameTemperature(int flameTemperature) {
+        this.flameTemperature = flameTemperature;
+    }
+
+    public void setFlameColor(String flameColor) {
+        this.flameColor = flameColor;
+    }
+
+    // methods, minimum of 2.
+   public int inferno(int xp) {
+        return xp + 60;
+   }
+
+    public int flameThrower(int xp) {
+        return xp + 22;
+    }
+
+    // override methods, minimum of 1.
+    @Override
+    public String eats(String food) {
+        return "ImmpImmp eating: " + food;
+    }
+
+    @Override
+    public String speaks(String sound) {
+        return sound;
+    }
 }
--- Pokemon-super/Src/PokemonSuper.java
+++ Pokemon-super/Src/PokemonSuper.java
@@ -1,6 +1,73 @@
 abstract class PokemonSuper {
+    // variables , minimum of 2 private.
+    private String name;
+    private int level;
+    private int hp;
+    private int xp;
+    private String food;
+    private String sound;
 
+    // constructor, minimum of 1.
+    public PokemonSuper(String name, int level, int hp, int xp, String food, String sound) {
+        this.name = name;
+        this.level = level;
+        this.hp = hp;
+        this.xp = xp;
+        this.food = food;
+        this.sound = sound;
+    }
 
+    // getters, minimum of 2.
+    public String getName() {
+        return name;
+    }
 
+    public int getLevel() {
+        return level;
+    }
 
+    public int getHp() {
+        return hp;
+    }
+
+    public int getXp() {
+        return xp;
+    }
+
+    public String getFood() {
+        return food;
+    }
+
+    public String getSound() {
+        return sound;
+    }
+
+    // Setters, minimum of 2.
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public void setHp(int hp) {
+        this.hp = hp;
+    }
+
+    public void setXp(int xp) {
+        this.xp = xp;
+    }
+
+    public void setFood(String food) {
+        this.food = food;
+    }
+
+    public void setSound(String sound) {
+        this.sound = sound;
+    }
+
+    // methods, minimum of 2.
+    public abstract String eats(String food);
+    public abstract String speaks(String sound);
 }
